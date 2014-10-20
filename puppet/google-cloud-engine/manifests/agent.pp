33f50fc514b4fc45b383c310cfb6fa4ae0090fa2
Marcus Gomes <mvp.gomes1989@gmail.com>
2014-10-20 22:21:58 +0100

    Add firewall rules to the puppet-agent.

--- puppet/google-cloud-engine/manifests/agent.pp
+++ puppet/google-cloud-engine/manifests/agent.pp
@@ -7,7 +7,7 @@ gce_instance { 'cotagent':
   zone           => 'europe-west1-b',
   machine_type   => 'g1-small',
   network        => 'default',
-  image          => 'projects/debian-cloud/global/images/backports-debian-7-wheezy-v20140924',
+  image          => 'projects/debian-cloud/global/images/debian-7-wheezy-v20141017',
   startupscript  => 'pe-simplified-agent.sh',
   metadata       => {
     'pe_role'    => 'agent',
@@ -15,4 +15,34 @@ gce_instance { 'cotagent':
     'pe_version' => '3.3.1',
   },
   tags           => ['puppet', 'pe-agent'],
+}
+
+gce_firewall { 'allow-http':
+  ensure      => present,
+  network     => 'default',
+  description => 'Allow incoming http connections.',
+  allowed     => 'tcp:80'
+}
+
+gce_httphealthcheck { 'basic-http':
+  ensure  => present,
+  require => Gce_instance['cotagent'],
+  description  => 'basic http health check',
+}
+
+gce_targetpool { 'web-pool':
+    ensure        => present,
+    require       => Gce_httphealthcheck['basic-http'],
+    health_checks => 'basic-http',
+    instances     => 'europe-west1-b/web1,europe-west1-a/web2',
+    region        => 'europe-west1',
+}
+
+gce_forwardingrule { 'web-rule':
+    ensure       => present,
+    require      => Gce_targetpool['web-pool'],
+    description  => 'Forward HTTP to web instances',
+    port_range   => '80',
+    region       => 'europe-west1',
+    target       => 'web-pool',
 }
